bcae5f3304c6133492f3e4b2512b865e14fe3449
fschipiura <f.schipiura@hotmail.com>
2019-01-26 17:40:02 -0200

    script da bolinha <3

---
+++ Assets/Scripts/bolinha.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bolinha : MonoBehaviour
+{
+    TargetJoint2D mola;
+    Vector2 cursor;
+
+    private void Start()
+    {
+        mola = transform.GetComponent<TargetJoint2D>();
+
+        mola.enabled = false;
+    }
+
+    private void Update()
+    {
+        cursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            mola.target = cursor;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        mola.enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        mola.enabled = false;
+    }
+
+
+}
