6f86f5a01657dd8d6e57d217e0181c33024a4f39
jeankarax <karaxjr@gmail.com>
2019-01-27 14:31:37 -0200

    casa

---
+++ Assets/Scripts/SFXScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXScript : MonoBehaviour {
+
+    public static AudioClip gato1, gato2, romrom;
+    static AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        gato1 = Resources.Load<AudioClip>("gato 1");
+        gato2 = Resources.Load<AudioClip>("gato 2");
+        romrom = Resources.Load<AudioClip>("romrom");
+
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void PlaySound(string sfx)
+    {
+        switch (sfx)
+        {
+            case "gato1":
+                audioSource.Stop();
+                audioSource.PlayOneShot(gato1);
+                break;
+            case "gato2":
+                audioSource.Stop();
+                audioSource.PlayOneShot(gato2);
+                break;
+            case "romrom":
+                audioSource.Stop();
+                audioSource.PlayOneShot(romrom);
+                break;
+        }
+    }
+}
--- Assets/Scripts/gato.cs
+++ Assets/Scripts/gato.cs
@@ -1,12 +1,42 @@
 ﻿
 using UnityEngine;
 
-public class gato : MonoBehaviour {
-
+public class gato : MonoBehaviour
+{
     public float health;
 
-    private Rigidbody2D rigidbody2d;
-    public Transform catTransform;
+    private Rigidbody2D rigidbody;
+    public Animator anim;
+
+    public float tamanhoDoGato;
+
+    float tempoIdleInicial;
+    bool idleInicial;
+
+    jarroDePetiscos jarroDePetiscos;
+    [HideInInspector] public Transform petiscoTransf;
+    public float TempoAnimaçComendoTotal;
+    float tempoAnimaçComendoAtual;
+
+    bolinha brinquedo;
+    GameObject brinquedoObj;
+    float tempoAtençãoBrinquedoAtual;
+    public float tempoAtençãoBrinquedoTotal;
+    public float TempoAnimaçBrinksTotal;
+    float tempoAnimaçBrinksAtual;
+
+    poteDeRação poteDeRação;
+    GameObject poteDeRaçãoObj;
+    public float tempoAnimaçComerTotal;
+    float tempoAnimaçComerAtual;
+
+    int direçAleatVagar;
+    public float tempoDeVagarTotal;
+    float tempoDeVagarAtual;
+
+    float tempoDeMudarAção;
+    bool vagando, brincando, comendo, petisco;
+    bool praticandoAção;
 
     private float catMovementSpeed;
     public bool isWalking;
@@ -15,69 +45,248 @@ public class gato : MonoBehaviour {
     private float waitTime;
     private float waitCounter;
     private int walkDirection;
-    public Vector3 currRot;
+    public Vector2 rotaçãoAtual;
 
-    // Use this for initialization
-    void Start () {
-        rigidbody2d = this.GetComponent<Rigidbody2D>();
-        catTransform = this.transform;
+    void Start()
+    {
+        jarroDePetiscos = GameObject.FindGameObjectWithTag("jarroDePetiscos").GetComponent<jarroDePetiscos>();
+        brinquedoObj = GameObject.FindGameObjectWithTag("Brinquedo");
+        brinquedo = brinquedoObj.GetComponent<bolinha>();
+        poteDeRaçãoObj = GameObject.FindGameObjectWithTag("Tijela");
+        poteDeRação = poteDeRaçãoObj.GetComponent<poteDeRação>();
+        anim = this.GetComponent<Animator>();
+
+        tempoIdleInicial = 3;
+
+        rigidbody = transform.GetComponent<Rigidbody2D>();
         waitCounter = 1;
         walkCounter = 2;
         walkDirection = 1;
-        currRot = catTransform.eulerAngles;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        Vector2 myVel = rigidbody2d.velocity;
-        myVel.x = catMovementSpeed;
-        rigidbody2d.velocity = myVel;
-       
-
-        if (isWalking)
-        {
-            walkCounter -= Time.deltaTime;
-            switch (walkDirection)
-            {
-                case 0:                
-                    rigidbody2d.velocity = new Vector2(1, 0);
-                    currRot.y = 180;
-                    catTransform.eulerAngles = currRot;
-                    break;
-                case 1:
-                    rigidbody2d.velocity = new Vector2(-1, 0);
-                    currRot.y = 0;
-                    catTransform.eulerAngles = currRot;
-                    break;
-            }
-        
-            if (walkCounter < 0)
-            {
-                isWalking = false;
-                walkCounter = 2;
-            }
-        }
-        else
-        {
-            waitCounter -= Time.deltaTime;
-            rigidbody2d.velocity =  Vector2.zero;
-            if(waitCounter < 0)
-            {
-                ChooseDirection();
-                waitCounter = 1;
-            }
-        }
-
+        rotaçãoAtual = transform.eulerAngles;
     }
 
-    public void ChooseDirection()
-    {      
-        walkDirection = Random.Range(0, 2);
-        isWalking = true;
-        if(gerenteGeral.saúdeDoGato == 0)
+    void Update()
+    {
+        tempoDeMudarAção -= Time.deltaTime;
+
+        tempoIdleInicial -= Time.deltaTime;
+        if (tempoIdleInicial < 0)
         {
-            currRot.z = 90;
+            idleInicial = true;
+        }
+        if (idleInicial)
+        {
+            //se houver algum petisco, o gato vai até ele e come
+            if ((tempoDeMudarAção <= 0 && jarroDePetiscos.petiscoNoChão && !brincando && !comendo && !vagando) || petisco)
+            {
+                petisco = true;
+
+                if (Vector2.Distance(transform.position, jarroDePetiscos.petiscoInstanciado.position) > tamanhoDoGato)
+                {
+                    //muda sprite para animação de andando
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isEating", false);
+                    anim.SetBool("isPlaying", false);
+                    anim.SetBool("isIdle", false);
+
+                    if (transform.position.x - jarroDePetiscos.petiscoInstanciado.position.x < 0)
+                    {
+                        rigidbody.velocity = new Vector2(3, 0);
+                        rotaçãoAtual.y = 180;
+                        transform.eulerAngles = rotaçãoAtual;
+                    }
+                    else
+                    {
+                        rigidbody.velocity = new Vector2(-3, 0);
+                        rotaçãoAtual.y = 0;
+                        transform.eulerAngles = rotaçãoAtual;
+                    }
+
+                    if (tempoAnimaçComendoAtual != TempoAnimaçComendoTotal)
+                    {
+                        tempoAnimaçComendoAtual = TempoAnimaçComendoTotal;
+                    }
+                }
+                else if (petiscoTransf != null && tempoAnimaçComendoAtual > 0)
+                {
+                    tempoAnimaçComendoAtual -= Time.deltaTime;
+
+                    //muda sprite para animação de comendo
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isEating", true);
+                    anim.SetBool("isPlaying", false);
+                    anim.SetBool("isIdle", false);
+                }
+                else if (petiscoTransf != null && tempoAnimaçComendoAtual <= 0)
+                {
+                    Destroy(petiscoTransf.gameObject);
+
+                    petisco = false;
+
+                    tempoDeMudarAção = 1.5f;
+
+                    jarroDePetiscos.petiscoNoChão = false;
+
+                    //adiciona pontos de saúde ao gato
+
+                    //muda sprite para idle
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isEating", false);
+                    anim.SetBool("isPlaying", false);
+                    anim.SetBool("isIdle", true);
+                    SFXScript.PlaySound("romrom");
+                }
+            }
+            //se a bolinha estiver em movimento, o gato começa a brincar com ela
+            else if ((tempoDeMudarAção <= 0 && brinquedo.emMovimento && !petisco && !comendo /*&& !vagando*/ && gerenteGeral.saciaçãodoDoGato > 25) || brincando)
+            {
+                brincando = true;
+                SFXScript.PlaySound("romrom");
+
+                /*tempoAtençãoBrinquedoAtual -= Time.deltaTime;
+
+                if (brinquedo.emMovimento)
+                {
+                    tempoAtençãoBrinquedoAtual = tempoAtençãoBrinquedoTotal;
+                }*/
+
+                if (Vector2.Distance(transform.position, brinquedoObj.transform.position) > tamanhoDoGato)
+                {
+                    //muda sprite para animação de andando
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isEating", false);
+                    anim.SetBool("isPlaying", false);
+                    anim.SetBool("isIdle", false);
+
+
+                    if (transform.position.x - brinquedoObj.transform.position.x < 0)
+                    {
+                        rigidbody.velocity = new Vector2(5, 0);
+                        rotaçãoAtual.y = 180;
+                        transform.eulerAngles = rotaçãoAtual;
+                    }
+                    else
+                    {
+                        rigidbody.velocity = new Vector2(-5, 0);
+                        rotaçãoAtual.y = 0;
+                        transform.eulerAngles = rotaçãoAtual;
+                    }
+
+                    if (tempoAnimaçBrinksAtual != TempoAnimaçBrinksTotal)
+                    {
+                        tempoAnimaçBrinksAtual = TempoAnimaçBrinksTotal;
+                    }
+                }
+                else if (tempoAnimaçBrinksAtual > 0)
+                {
+                    tempoAnimaçBrinksAtual -= Time.deltaTime;
+
+                    //muda sprite para animação de brincando
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isEating", false);
+                    anim.SetBool("isPlaying", true);
+                    anim.SetBool("isIdle", false);
+                }
+                else if (tempoAnimaçBrinksAtual <= 0)
+                {
+                    brinquedo.pular();
+
+                    brincando = false;
+
+                    tempoDeMudarAção = 1.5f;
+                }
+            }
+            else if ((tempoDeMudarAção <= 0 && poteDeRação.temRação && !brinquedo.emMovimento && !petisco && gerenteGeral.saciaçãodoDoGato <= 50) || comendo)
+            {
+                comendo = true;
+
+                if (Vector2.Distance(transform.position, poteDeRaçãoObj.transform.position) > tamanhoDoGato)
+                {
+                    //muda sprite para animação de andando
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isEating", false);
+                    anim.SetBool("isPlaying", false);
+                    anim.SetBool("isIdle", false);
+
+                    if (transform.position.x - poteDeRaçãoObj.transform.position.x < 0)
+                    {
+                        rigidbody.velocity = new Vector2(3, 0);
+                        rotaçãoAtual.y = 180;
+                        transform.eulerAngles = rotaçãoAtual;
+                    }
+                    else
+                    {
+                        rigidbody.velocity = new Vector2(-3, 0);
+                        rotaçãoAtual.y = 0;
+                        transform.eulerAngles = rotaçãoAtual;
+                    }
+                }
+                else if (tempoAnimaçComendoAtual > 0)
+                {
+                    tempoAnimaçBrinksAtual -= Time.deltaTime;
+
+                    //muda sprite para animação de comendo
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isEating", true);
+                    anim.SetBool("isPlaying", false);
+                    anim.SetBool("isIdle", false);
+                }
+                else if (tempoAnimaçComendoAtual <= 0)
+                {
+                    poteDeRação.gatoComer();
+
+                    comendo = false;
+
+                    tempoDeMudarAção = 1.5f;
+                }
+            }
+            //gato fica vagando
+            else if ((tempoDeMudarAção <= 0 && !brinquedo.emMovimento && !petisco && !comendo && gerenteGeral.saciaçãodoDoGato > 25) || vagando)
+            {
+                if (!vagando)
+                {
+                    direçAleatVagar = Random.Range(0, 2);
+
+                    tempoDeVagarAtual = tempoDeVagarTotal;
+                }
+
+                vagando = true;
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isEating", false);
+                anim.SetBool("isPlaying", false);
+                anim.SetBool("isIdle", false);
+                SFXScript.PlaySound("gato1");
+
+                switch (direçAleatVagar)
+                {
+                    case 0:
+                        rigidbody.velocity = new Vector2(5, 0);
+                        rotaçãoAtual.y = 180;
+                        transform.eulerAngles = rotaçãoAtual;
+                        break;
+                    case 1:
+                        rigidbody.velocity = new Vector2(-5, 0);
+                        rotaçãoAtual.y = 0;
+                        transform.eulerAngles = rotaçãoAtual;
+                        break;
+                }
+
+                if (tempoDeVagarAtual > 0)
+                {
+                    tempoDeVagarAtual -= Time.deltaTime;
+                }
+
+                if (tempoDeVagarAtual <= 0)
+                {
+                    vagando = false;
+
+                    tempoDeMudarAção = 2f;
+                }
+            }
+            else
+            {
+                //gato vai até o pote e fica miando
+            }
         }
     }
 }
